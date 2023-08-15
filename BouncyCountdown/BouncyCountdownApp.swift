85e4cd8fb8b75d23871a170b514fbebb0aa9eca5
usagimaru <usagimaruma@gmail.com>
2023-08-15 13:06:49 +0900

    Initial Commit

---
+++ BouncyCountdown/BouncyCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  BouncyCountdownApp.swift
+//  BouncyCountdown
+//
+//  Created by usagimaru on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct BouncyCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BouncyCountdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BouncyCountdown
+//
+//  Created by usagimaru on 2023/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
